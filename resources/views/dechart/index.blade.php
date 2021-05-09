37d41f724e53aa1e88b0de1593388d0e79c1347b
midota-phucdaivo <phucvd@midota.com>
2021-05-10 01:18:39 +0700

    update font

--- resources/views/dechart/index.blade.php
+++ resources/views/dechart/index.blade.php
@@ -145,10 +145,10 @@
     href="https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;ver=5.7.1&amp;display=swap"
     type="text/css" media="all">
   <link rel="preload stylesheet preconnect" as="style" id="elementor-icons-shared-0"
-    href="https://dechart.io/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.1"
+    href="{{ url('fontawesome-free-5.15.3-web/css/fontawesome.min.css?ver=5.15.1') }}
     type="text/css" media="all">
   <link rel="preload stylesheet preconnect" as="style" id="elementor-icons-fa-brands"
-    href="https://dechart.io/wp-content/plugins/elementor/assets/lib/font-awesome/css/brands.min.css?ver=5.15.1"
+    href="{{ url('fontawesome-free-5.15.3-web/css/brands.min.css?ver=5.15.1') }}
     type="text/css" media="all">
   <script type="text/javascript" src="https://dechart.io/wp-includes/js/jquery/jquery.min.js?ver=3.5.1"
     id="jquery-core-js"></script>

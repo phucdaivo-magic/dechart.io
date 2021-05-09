d79aee2109cccc5232e640977e024ea133edaa3b
midota-phucdaivo <phucvd@midota.com>
2021-05-10 00:58:48 +0700

    update url

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dechart.index');
 });
 Route::get('/dechart.io', function () {
     return view('dechart.index');
